ce31f42f3deef5f48c776a1e5a76988a77666223
Emerentius <emerentius@arcor.de>
2019-02-19 17:01:23 +0100

    use 7zip on windows

--- ci/package.sh
+++ ci/package.sh
@@ -2,7 +2,7 @@
 
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
 	mv target/release/rudoku rudoku.exe
-	zip "rudoku-$TRAVIS_TAG-$TRAVIS_OS_NAME-x86_64.zip" rudoku.exe
+	7z a "rudoku-$TRAVIS_TAG-$TRAVIS_OS_NAME-x86_64.zip" rudoku.exe
 else
 	mv target/release/rudoku rudoku
 	zip "rudoku-$TRAVIS_TAG-$TRAVIS_OS_NAME-x86_64.zip" rudoku
